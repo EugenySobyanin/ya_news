6aaa713b963edd6d380a7a2f3f671b56a79e27ca
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-06-11 15:52:25 +0300

    Тест контента.

--- news/tests/test_trial.py
+++ news/tests/test_trial.py
@@ -1,14 +1,36 @@
+import unittest
+
 # news/tests/test_trial.py
 from django.test import TestCase
 
+# Импортируем модель, чтобы работать с ней в тестах.
+from news.models import News
 
-class Test(TestCase):
+# news/tests/test_trial.py
+from django.test import TestCase
 
-    def test_example_success(self):
-        self.assertTrue(True)  # Этот тест всегда будет проходить успешно.
+from news.models import News
 
 
-class YetAnotherTest(TestCase):
+@unittest.skip()
+class TestNews(TestCase):
+    # Все нужные переменные сохраняем в атрибуты класса.
+    TITLE = 'Заголовок новости'
+    TEXT = 'Тестовый текст'
+    
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            # При создании объекта обращаемся к константам класса через cls.
+            title=cls.TITLE,
+            text=cls.TEXT,
+        )
 
-    def test_example_fails(self):
-        self.assertTrue(False)  # Этот тест всегда будет проваливаться.
+    def test_successful_creation(self):
+        news_count = News.objects.count()
+        self.assertEqual(news_count, 1)
+
+    def test_title(self):
+        # Чтобы проверить равенство с константой -
+        # обращаемся к ней через self, а не через cls:
+        self.assertEqual(self.news.title, self.TITLE)
